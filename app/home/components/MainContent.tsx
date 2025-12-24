2f2e1cb716cf2a66fa824f56f61b814a171e6c74
Isaac Malak <isaacmalak123@gmail.com>
2025-12-24 22:18:01 +0200

    Refactor code structure for improved readability and maintainability

--- app/home/components/MainContent.tsx
+++ app/home/components/MainContent.tsx
@@ -5,20 +5,22 @@ import { ProfileNavBar } from "./ProfileNavBar";
 import Particles from "@/components/Particles";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
-
+import { Fira_Code } from "next/font/google";
+import { Source_Code_Pro } from "next/font/google";
+import Image from "next/image";
+const sourceCodePro = Source_Code_Pro({
+  weight: "700",
+  subsets: ["latin"],
+});
 export function MainContent() {
   const boxRef = useRef(null);
   const bgRef = useRef(null);
   useGSAP(() => {
     gsap
       .timeline()
-      .to(
-        bgRef.current,
-
-        {
-          backgroundColor: "black",
-        }
-      )
+      .to(bgRef.current, {
+        backgroundColor: "black",
+      })
       .fromTo(
         boxRef.current,
         {
@@ -27,22 +29,43 @@ export function MainContent() {
         {
           opacity: 1,
           duration: 2,
-
           ease: "power3.out",
         }
       );
   });
   return (
-    <div ref={bgRef} className="bg-white w-full h-full">
-      <div ref={boxRef} className="w-full h-full relative">
+    <div ref={bgRef} className="bg-white w-full h-screen flex flex-col">
+      <ProfileNavBar />
+      <div ref={boxRef} className="w-full  flex-1 relative">
         <Particles
           particleColors={undefined}
-          className=""
+          className="absolute inset-0 pointer-events-none"
           particleBaseSize={100}
           particleCount={300}
         />
-        <div className="fixed top-0 left-0 w-full">
-          <ProfileNavBar />
+        <div className=" flex-row items-center px-20 w-full h-full relative top-0 left-0 flex">
+          <section className="w-1/2 h-1/2   ">
+            <h1 className="text-5xl font-extrabold text-white mb-6 drop-shadow-lg">
+              <span
+                className={`font-extrabold tracking-widest ${sourceCodePro.className}`}
+              >
+                Isaac Malak
+              </span>
+            </h1>
+            <p className="text-lg text-white/90 font">
+              I&apos;m a passionate software developer specializing in building
+              exceptional digital experiences. Welcome to my portfolio!
+            </p>
+          </section>
+          <section className="w-1/2 h-1/2 ">
+            <Image
+              src="/images/profile3.png"
+              alt="Profile Picture"
+              width={400}
+              height={400}
+              className="rounded-full mx-auto shadow-lg"
+            />
+          </section>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,7 @@ export default function Home() {
           }}
         />
       ) : (
-        
-          <MainContent />
+        <MainContent />
       )}
     </main>
   );
