668d3ccb45cab19b7f0c20ccb6adef7e54a91b3d
Isaac Malak <isaacmalak123@gmail.com>
2026-01-26 17:56:24 +0200

    Refactor ImageDialogue component to use function declaration instead of forwardRef; improve readability and maintainability

--- components/dialogues/ImageDialogue.tsx
+++ components/dialogues/ImageDialogue.tsx
@@ -1,28 +1,36 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
-import { forwardRef, Ref } from "react";
+import { Ref } from "react";
 
-export const ImageDialogue = forwardRef<HTMLDialogElement, { image: string }>(
-  function ImageDialogue({ image }, ref: Ref<HTMLDialogElement>) {
-    return (
-      <dialog
-        ref={ref}
-        className="backdrop:bg-black/40 m-auto border-0 bg-transparent "
-        onClick={(e) => {
-          console.log("closing");
-          (e.currentTarget as HTMLDialogElement).close();
-        }}
-      >
-        <div onClick={(e) => e.stopPropagation()}>
-          <Image
-            src={image}
-            width={1920}
-            height={1080}
-            alt=""
-            quality={100}
-            className=" max-h-[90vh] max-w-[90vw] "
-          />
-        </div>
-      </dialog>
-    );
-  }
-);
+export function ImageDialogue({
+  ref,
+  image,
+}: {
+  ref: Ref<HTMLDialogElement>;
+  image: string;
+}) {
+  useGSAP(() => {});
+  return (
+    <dialog
+      ref={ref}
+      className="backdrop:bg-black/40 m-auto border-0 bg-transparent "
+      onClick={(e) => {
+        console.log("closing");
+        (e.currentTarget as HTMLDialogElement).close();
+      }}
+    >
+      <div onClick={(e) => e.stopPropagation()}>
+        <Image
+          src={image}
+          width={1920}
+          height={1080}
+          alt=""
+          quality={100}
+          className=" max-h-[90vh] max-w-[90vw] "
+        />
+      </div>
+    </dialog>
+  );
+}
