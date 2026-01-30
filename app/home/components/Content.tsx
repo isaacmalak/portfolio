f57967e1e7c16bef8434c396762ebd88ae9fe21e
Isaac Malak <isaacmalak123@gmail.com>
2026-01-30 21:27:57 +0200

    Refactor Content component; adjust typography sizes and spacing for improved readability

--- app/home/components/Content.tsx
+++ app/home/components/Content.tsx
@@ -23,19 +23,19 @@ export function Content({
           className={`flex flex-col justify-center bg-red-500/0 py-10 md:h-1/2 md:w-full md:flex-1 md:justify-center md:pt-0 ${roboto_mono.className} px-2`}
         >
           <h1
-            className={`text-4xl font-bold tracking-widest text-white uppercase md:text-6xl`}
+            className={`text-2xl font-bold tracking-widest text-white uppercase md:text-6xl`}
           >
             Isaac Malak
           </h1>
-          <p className="font-mono text-white/80 md:text-2xl">
+          <p className="font-mono text-white/80">
             <span
-              className={`bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-400 bg-clip-text font-bold text-transparent md:text-3xl ${orbitron.className}`}
+              className={`bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-400 bg-clip-text text-xs font-bold text-transparent md:text-3xl ${orbitron.className}`}
             >
               Building apps, shipping code, and always learning.
             </span>
             <br />
             <span
-              className={`tracking-wider text-white/70 md:text-2xl ${orbitron.className}`}
+              className={`text-xs tracking-wider text-white/70 md:text-2xl ${orbitron.className}`}
             >
               Focused on clean design, smooth user experiences and
               <span ref={coffeeRef} className="italic">
@@ -44,18 +44,18 @@ export function Content({
               </span>
             </span>
             <br />
-            <span className="text-xl font-bold text-cyan-200 italic md:text-2xl">
+            <span className="text-sm font-bold text-cyan-200 italic md:text-2xl">
               Check out my projects below.
             </span>
           </p>
         </section>
-        <section className="flex w-full flex-col items-center justify-center bg-blue-700/0 md:h-1/2 md:flex-1 md:pt-0">
+        <section className="flex w-full flex-col items-center justify-center bg-blue-700/0 px-10 md:h-1/2 md:flex-1 md:px-0 md:pt-0">
           <Image
             src="/images/profile3.png"
             alt=""
             width={400}
             height={400}
-            className="h-auto rounded-full select-none"
+            className="rounded-full select-none md:h-auto"
           />
         </section>
       </div>
