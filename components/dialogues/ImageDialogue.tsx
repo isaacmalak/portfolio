6096f482573a71e8a3cb791de2d635523588949a
Isaac Malak <isaacmalak123@gmail.com>
2026-02-09 20:31:17 +0200

    fix: update ImageDialogue to reset offset on scale change; improve drag handling for image positioning

--- components/dialogues/ImageDialogue.tsx
+++ components/dialogues/ImageDialogue.tsx
@@ -19,6 +19,7 @@ export function ImageDialogue({
   const dialogRef = useRef<HTMLDialogElement>(null);
   const imageRef = useRef(null);
   const [scale, setScale] = useState(1);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   useImperativeHandle(ref, () => dialogRef.current!);
 
@@ -30,12 +31,27 @@ export function ImageDialogue({
         setScale((prev) => {
           const newScale = prev - delta[1] * 0.0009;
 
-          // Puts the limit of the scale between 0.95 and 2, this is to prevent the image from being too small or too big
-          return Math.min(Math.max(newScale, 0.95), 2);
+          // Puts the limit of the scale between 1 and 2, this is to prevent the image from being too small or too big
+          const scale = Math.min(Math.max(newScale, 1), 2);
+          if (scale == 1) {
+            setOffset({ x: 0, y: 0 });
+          }
+
+          return scale;
         });
       },
+      onDrag: ({ offset, event }) => {
+        event.preventDefault();
+        setOffset({ x: offset[0], y: offset[1] });
+      },
     },
-    { target: imageRef },
+    {
+      target: imageRef,
+      eventOptions: { passive: false },
+      drag: {
+        from: () => (scale === 1 ? [0, 0] : [offset.x, offset.y]),
+      },
+    },
   );
 
   const onOpen = () => {
@@ -80,6 +96,8 @@ export function ImageDialogue({
             opacity: 0,
             duration: 0.3,
             onComplete: () => {
+              setScale(1);
+              setOffset({ x: 0, y: 0 });
               setClosing(true);
               document.body.style.overflow = "";
               dialogRef.current?.close();
@@ -89,19 +107,21 @@ export function ImageDialogue({
       }}
     >
       <div
-        className="relative overflow-hidden rounded-xl bg-gray-900/30 transition-transform md:h-[90vh] md:w-[90vw] md:rounded-3xl"
+        className="relative touch-none overflow-hidden rounded-xl transition-transform md:h-[90vh] md:w-[90vw] md:rounded-3xl"
         ref={imageRef}
       >
         <Image
           src={image}
           style={{
             transform: `scale(${scale})`,
+            left: offset.x,
+            top: offset.y,
           }}
           width={1920}
           height={1080}
           alt=""
           quality={100}
-          className="rounded-xl duration-600 md:rounded-3xl"
+          className="relative rounded-xl duration-400 md:rounded-3xl"
         />
       </div>
     </dialog>
