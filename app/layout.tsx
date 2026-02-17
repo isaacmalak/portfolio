ff16033b4302f33bf87b9ee1b08176c59a6a1acd
Isaac Malak <isaacmalak123@gmail.com>
2026-02-17 12:07:08 +0200

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,24 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Isaac Malak",
-  description: "Created with love",
+  description: "Created with coffee",
+};
+// image that will appear on social media when sharing the website link
+const metaImageUrl = "/images/profile_colored.jpg";
+
+metadata.openGraph = {
+  images: [
+    {
+      url: metaImageUrl,
+      width: 1200,
+      height: 630,
+      alt: "Isaac Malak",
+    },
+  ],
+};
+metadata.twitter = {
+  card: "summary_large_image",
+  images: [metaImageUrl],
 };
 
 export default function RootLayout({
